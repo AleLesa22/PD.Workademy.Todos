3953df250afdda0339d2fa41b180cd7963640390
Aleksandar Vidakovic <aleksandar.vidakovic7422@gmail.com>
2022-11-02 13:42:48 +0100

    adding endpoints in CategoryService

--- PD.Workademy.Todo/src/Application/PD.Workademy.Todo.Application/Services/CategoryService.cs
+++ PD.Workademy.Todo/src/Application/PD.Workademy.Todo.Application/Services/CategoryService.cs
@@ -9,6 +9,34 @@ namespace PD.Workademy.Todo.Application.Services
 {
     public class CategoryService:ICategoryService
     {
+        public List<Category> AddCategory(Category request)
+        {
+            List<Category> categories = new()
+            {
+                new Category(new Guid("747394a8-3712-4743-a2d1-521d2df10223"), "Easy"),
+                new Category(new Guid("e7efd2aa-ceac-45af-9421-7cb0d95ab272"), "Medium"),
+                new Category(new Guid("c81382f5-55ae-4617-b911-9a48f9bb5892"), "Hard")
+            };
+            categories.Add(request);
+            return categories;
+        }
+
+        //DeleteCategory
+
+        public List<Category> DeleteCategory(Guid Id)
+        {
+            List<Category> categories = new()
+            {
+                new Category(new Guid("747394a8-3712-4743-a2d1-521d2df10223"), "Easy"),
+                new Category(new Guid("e7efd2aa-ceac-45af-9421-7cb0d95ab272"), "Medium"),
+                new Category(new Guid("c81382f5-55ae-4617-b911-9a48f9bb5892"), "Hard")
+            };
+            var categoryToDelete = categories.Find(x => x.Id == Id);
+            categories.Remove(categoryToDelete);
+            return categories;
+        }
+
+        //GetCategories
         public List<Category> GetCategories()
         {
             List<Category> categories = new()
@@ -18,6 +46,35 @@ namespace PD.Workademy.Todo.Application.Services
                 new Category(new Guid("c81382f5-55ae-4617-b911-9a48f9bb5892"), "Hard")
             };
             return categories;
+        }
+
+
+        //GetCategoryById
+        public Category GetCategoryById(Guid Id)
+        {
+            List<Category> categories = new()
+            {
+                new Category(new Guid("747394a8-3712-4743-a2d1-521d2df10223"), "Easy"),
+                new Category(new Guid("e7efd2aa-ceac-45af-9421-7cb0d95ab272"), "Medium"),
+                new Category(new Guid("c81382f5-55ae-4617-b911-9a48f9bb5892"), "Hard")
+            };
+            var categoryToFind = categories.Find(x => x.Id == Id);
+            return categoryToFind;
+        }
+
+        public Category UpdateCategory(Guid Id,Category request)
+        {
+            List<Category> categories = new()
+            {
+                new Category(new Guid("747394a8-3712-4743-a2d1-521d2df10223"), "Easy"),
+                new Category(new Guid("e7efd2aa-ceac-45af-9421-7cb0d95ab272"), "Medium"),
+                new Category(new Guid("c81382f5-55ae-4617-b911-9a48f9bb5892"), "Hard")
+            };
+            var categoryToUpdate = categories.Find(x => x.Id == Id);
+            categoryToUpdate.Id = request.Id;
+            categoryToUpdate.Name = request.Name;
+
+            return categoryToUpdate;
         }
     }
 }
--- PD.Workademy.Todo/src/Application/PD.Workademy.Todo.Application/Services/ICategoryService.cs
+++ PD.Workademy.Todo/src/Application/PD.Workademy.Todo.Application/Services/ICategoryService.cs
@@ -5,5 +5,9 @@ namespace PD.Workademy.Todo.Application.Services
     public interface ICategoryService
     {
         List<Category> GetCategories();
+        List<Category> AddCategory(Category request);  
+        List<Category> DeleteCategory(Guid Id);
+        Category GetCategoryById(Guid Id);
+        Category UpdateCategory(Guid Id,Category request);
     }
 }
