1ebddfa1f161058e39d04b042471d936296d6e3e
Aleksandar Vidakovic <aleksandar.vidakovic7422@gmail.com>
2022-11-04 13:22:14 +0100

    adding controllers for get all

--- PD.Workademy.Todo/src/Web/PD.Workademy.Todo.Web/Controllers/UsersController.cs
+++ PD.Workademy.Todo/src/Web/PD.Workademy.Todo.Web/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PD.Workademy.Todo.Application.Services;
 using PD.Workademy.Todo.Web.ApiModels;
 
 namespace PD.Workademy.Todo.Web.Controllers
@@ -7,20 +8,17 @@ namespace PD.Workademy.Todo.Web.Controllers
     
     public class UsersController : ApiBaseController
     {
-        [HttpGet]
-        public async Task<ActionResult> GetAllOfUsersAsync()
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
         {
-            List<UserDTO> GetAllOfUsers()
-            {
-                List<UserDTO> users = new List<UserDTO>()
-                    {
-                     new UserDTO(new Guid("747394a8-3712-4743-a2d1-521d2df10223"), "Aleksandar","Vidakovic"),
-                     new UserDTO(new Guid("e7efd2aa-ceac-45af-9421-7cb0d95ab272"), "Matija","Davidovic"),
-                     new UserDTO(new Guid("c81382f5-55ae-4617-b911-9a48f9bb5892"), "Nikola","Djokic")
-                    };
-                return users;
-            }
-            return Ok(GetAllOfUsers());
+            _userService = userService;
+        }
+
+        //ADD USER
+        [HttpGet]
+        public async Task<ActionResult> GetAllUsersAsync()
+        {
+            return Ok(_userService.GetUsers());
         }
     }
 }
